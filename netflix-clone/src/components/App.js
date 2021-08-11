5fe8ad8a871b87f95b6ebbcc817e22b437ea60c2
omar-etman <omartareketman@gmail.com>
2021-08-11 14:51:09 +0200

    Optimize the Row.js component
    -for efficient rendering use the move.id
      key so that only changes are re
      rendered in case of any.
    -erase the pre provided App.css
      content from the create react app,
      and for starters set margins to 0.
    -pass the isLargeRow boolean as a
      prop to be used as a condition to
      map either the posterpath or the
      backdrop path for the img
    -set the class for posters in the
     component to have another class
     added (row__posterLarge) if the
     component has the isLargeRow to be
     true , to which a bigger height and
     hover transform zoom scale effect are
     applied.
    -apply a left margin on the row div so
      it wouldn't touch the sides
    -plot the next 2 components

--- netflix-clone/src/components/App.js
+++ netflix-clone/src/components/App.js
@@ -5,8 +5,12 @@ import requests from '../utils/requests';
 function App() {
   return (
     <div className="App">
-      <h1>let's get that thing rollin</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      {/* Nav */}
+      {/* Banner */}
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -4,18 +4,15 @@ import  '../Row.css';
 
 const imgBaseURL = "https://image.tmdb.org/t/p/original/"
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     // a conditonal code snippet
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-
             setMovies(request.data.results);
-
             return request;
-
         }
         fetchData();
     }, [fetchUrl])
@@ -27,7 +24,11 @@ function Row({title, fetchUrl}) {
            <h2>{title}</h2>
            <div className="row__posters">
                 {movies.map(movie => (
-                    <img className="row__poster" src={`${imgBaseURL}${movie.poster_path}`} alt={movie.name}/>
+                    <img 
+                        key={movie.id}
+                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                        src={`${imgBaseURL}${isLargeRow ? movie.poster_path :movie.backdrop_path}`} alt={movie.name}
+                    />
                 ))}
            </div>
         </div>
