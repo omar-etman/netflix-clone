0cc5caa2c0c04c8fedb6fb53eba2c0399d491319
omar-etman <omartareketman@gmail.com>
2021-08-11 13:47:45 +0200

    Create Row.js (append to the row build process)

---
+++ netflix-clone/src/components/Row.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import axios  from '../utils/axios';
+import  '../Row.css';
+
+const imgBaseURL = "https://image.tmdb.org/t/p/original/"
+
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+
+    // a conditonal code snippet
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+
+            setMovies(request.data.results);
+
+            return request;
+
+        }
+        fetchData();
+    }, [fetchUrl])
+
+    console.log(movies)
+
+    return (
+        <div className="row">
+           <h2>{title}</h2>
+           <div className="row__posters">
+                {movies.map(movie => (
+                    <img className="row__poster" src={`${imgBaseURL}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+           </div>
+        </div>
+    )
+}
+
+export default Row
