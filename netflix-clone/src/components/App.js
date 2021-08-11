ca952ee154696968e4f1256cd0cd3a63e9c298f9
omar-etman <omartareketman@gmail.com>
2021-08-11 13:46:32 +0200

    Build the row component (preliminary)
    -setup the components in place
    -create the Row.js component file
    -pass the title as prop to the function
    -setup state via useState hook
    -import the requests object from the
     requests file and assign each request
     to its row component as props.
    -within useEffect we will make an
      async function that will run whenever
      the row is loaded
    -import the axios file which will get the
      baseUrl and append to it the fetchUrl
      prop as coded in its file via axios.get
      and log the request made
    -use the required piece of the response as state , by appending it to setMovies (via useState hook)
    -after getting the required results, map
     them in a container div (this will need
     the baseURL for images concatnated
     upon the img that was brought from
     the movies.poster_path
    - setup a Row.css file to adjust the
      posters display

--- netflix-clone/src/components/App.js
+++ netflix-clone/src/components/App.js
@@ -1,9 +1,20 @@
 import '../App.css';
+import Row from"./Row"
+import requests from '../utils/requests';
 
 function App() {
   return (
     <div className="App">
       <h1>let's get that thing rollin</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
     </div>
   );
 }
